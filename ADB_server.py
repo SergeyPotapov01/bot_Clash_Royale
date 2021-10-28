e48f5b0dc1b19e574b8447281027df2df8d451ba
SergeyPotapov01 <88240890+SergeyPotapov01@users.noreply.github.com>
2021-10-28 23:48:04 +0500

    Add files via upload

---
+++ ADB_server.py
@@ -0,0 +1,39 @@
+import os
+
+from ppadb.client import Client as AdbClient
+import time
+
+class ADB_server:
+    def __init__(self):
+        os.system(r'"C:\Program Files (x86)\Nox\bin\adb.exe start-server"')
+        os.system(r'"C:\Program Files (x86)\Nox\bin\adb.exe connect 127.0.0.1:62001"')
+        client = AdbClient(host="127.0.0.1", port=5037)
+        self.devices = client.devices()
+        self.device = self.devices[0]
+
+    def cheakCR(self):
+        packages = self.device.shell('pm list packages').split('\r\n')
+        for package in packages:
+            if package == 'package:com.supercell.clashroyale':
+                return True
+        return False
+
+    def openCR(self):
+        self.device.shell('monkey -p com.supercell.clashroyale -v 15')
+
+    def closeCR(self):
+        self.device.shell('am force-stop com.supercell.clashroyale')
+
+    def click(self, x: int, y: int):
+        self.device.shell(f'input tap {str(x)} {(str(y))}')
+
+    def getScreen(self):
+        return self.device.screencap()
+
+if __name__ == '__main__':
+    z = ADB_server()
+    c = []
+    v = time.time()
+    for i in range(10):
+        print(z.click(100, 100))
+    print(time.time() - v)
--- main.py
+++ main.py
@@ -1,49 +1,31 @@
-import os
 import time
 
-from ppadb.client import Client as AdbClient
+from ADB_server import ADB_server
 
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
-class ADB_server:
+ADB_server = ADB_server()
+
+class Bot:
     def __init__(self):
-        #os.system(r'"C:\Program Files (x86)\Nox\bin\Nox.exe"')
-        os.system(r'"ADB\adb.exe start-server"')
-        os.system(r'"ADB\adb.exe connect 127.0.0.1:62001"')
-        client = AdbClient(host="127.0.0.1", port=5037)
-        self.device = client.devices()[0]
-
-    def _cheakCR(self):
-        packages = self.device.shell('pm list packages').split('\r\n')
-        for package in packages:
-            if package == 'package:com.supercell.clashroyale':
-                return True
-        return False
-
-    def _openCR(self):
-        self.device.shell('monkey -p com.supercell.clashroyale -v 15')
-
-    def _closeCR(self):
-        self.device.shell('am force-stop com.supercell.clashroyale')
+        self.ADB = ADB_server
+        self._inGame = False
+        self._inBattle = False
+        self._battleMode = None
 
     def battle2X2(self):
-        self.device.shell('input tap 460 560')
-        time.sleep(2)
-        self.device.shell('input tap 480 580')
+        self.ADB.click(460, 560)
         time.sleep(1)
-        # тут могла выскочить иконка на лимит наград, надо ее подтвердить
-        while True:
-            pass # тут логика боя, надо продумывать
+        self.ADB.click(480, 580)
 
     def battleGlobal(self):
-        self.device.shell('input tap 260 560')
-        time.sleep(1)
-        # тут могла выскочить иконка на лимит наград, надо ее подтвердить
-        while True:
-            pass # тут логика боя, надо продумывать
+        self.ADB.click(260, 560)
 
-if __name__ == '__main__':
-    z = ADB_server()
-    z._closeCR()
-    print(z._cheakCR())
+    def selectCard(self, number):
+        self.ADB.click(200 + number * 90, 770)
+
+    def main(self):
+        while True:
+            q = input('1. Battle Global\n 2. Battle 2X2\n 3. Exit')
+
